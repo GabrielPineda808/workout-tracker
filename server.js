34a317bda6dca8a98f3f94d4d9fa2110fe25587b
Gabriel Pineda <gabepineda6@gmail.com>
2020-11-07 22:32:19 -0800

    deploying

--- server.js
+++ server.js
@@ -10,10 +10,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/workouts',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
+
 
 app.use('/api',require("./routes/api-routes"))
 app.use('/',require("./routes/html-routes"))
